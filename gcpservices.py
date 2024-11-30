3d57f7cccce8ee1c9c1c7bd1ac7fc8336cf2f090
jigyanshrathore <122160447+jigyanshrathore@users.noreply.github.com>
2024-11-30 18:45:32 +0530

    Update gcpservices.py

--- gcpservices.py
+++ gcpservices.py
@@ -3,7 +3,7 @@ from google.cloud import vision
 from google.cloud import speech
 from google.cloud import storage
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "C:/Users/HP/Desktop/aivideotagging/majestic-cairn-442720-q2-1a0c8e4b7aba.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "apipath.json"
 
 vision_client = vision.ImageAnnotatorClient()
 
